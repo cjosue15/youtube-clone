d601eaebb5e3ff302f3427edf30959b85eaba413
Carlos Josue <carlosjosuemn98@gmail.com>
2021-11-18 23:26:54 -0500

    refactor app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './pages/home/home.module';
+import { NavbarModule } from './core/components/navbar/navbar.module';
+import { SidebarModule } from './core/components/sidebar/sidebar.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HomeModule, NavbarModule, SidebarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

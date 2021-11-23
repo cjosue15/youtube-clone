242368dc605cc8da1d85fe60163e3ed72fe93a10
Carlos Josue <carlosjosuemn98@gmail.com>
2021-11-23 15:41:31 -0500

    sidebar finished

---
+++ src/app/core/components/sidebar/constants/sidebar.constants.ts
@@ -0,0 +1,125 @@
+import { ISideBar } from '../models/sidebar.model';
+
+export const sidebarData: ISideBar[] = [
+  {
+    section: '',
+    items: [
+      {
+        name: 'Principal',
+        icon: 'assets/images/sidebar/principal.svg',
+        renderInHide: true,
+      },
+      {
+        name: 'Explorar',
+        icon: 'assets/images/sidebar/principal.svg',
+        renderInHide: true,
+      },
+      {
+        name: 'Suscripciones',
+        icon: 'assets/images/sidebar/principal.svg',
+        renderInHide: true,
+      },
+    ],
+  },
+  {
+    section: '',
+    items: [
+      {
+        name: 'Biblioteca',
+        icon: 'assets/images/sidebar/principal.svg',
+        renderInHide: true,
+      },
+      {
+        name: 'Historial',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Tus videos',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Ver más tarde',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Videos que me gustan',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Mostrar más',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+    ],
+  },
+  {
+    section: 'SUSCRIPCIONES',
+    items: [
+      {
+        name: 'Code Pioners',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'RT',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Cracks',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Fernando Herrrera',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Mostrar 50 más',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+    ],
+  },
+  {
+    section: 'MAS DE YOUTUBE',
+    items: [
+      {
+        name: 'Youtube Premium',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Videojuegos',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'En vivo',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Aprendizaje',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Deportes',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+    ],
+  },
+  {
+    section: '',
+    items: [
+      {
+        name: 'Configuración',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Historial de denuncias',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Ayuda',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+      {
+        name: 'Enviar comentarios',
+        icon: 'assets/images/sidebar/principal.svg',
+      },
+    ],
+  },
+];
--- src/app/core/components/sidebar/sidebar.module.ts
+++ src/app/core/components/sidebar/sidebar.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar.component';
-
-
+import { IconModule } from '../../../shared/components/icon/icon.module';
 
 @NgModule({
-  declarations: [
-    SidebarComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SidebarComponent],
+  imports: [CommonModule, IconModule],
+  exports: [SidebarComponent],
 })
-export class SidebarModule { }
+export class SidebarModule {}

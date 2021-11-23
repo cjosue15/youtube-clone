0aac52bf9fa863008c0447445146c0c2a8224f0a
Carlos Josue <carlosjosuemn98@gmail.com>
2021-11-23 15:42:03 -0500

    navbar and service

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SideNavService } from '../../services/side-nav.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
-  constructor() {}
+export class NavbarComponent {
+  constructor(private sideNavService: SideNavService) {}
 
-  ngOnInit(): void {}
+  async changeMenu() {
+    const isVisible = await this.sideNavService.visibilityOfMenu$
+      .pipe(take(1))
+      .toPromise();
+    this.sideNavService.showMenu = isVisible ? false : true;
+  }
 }
